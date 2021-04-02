3e220b18ee00e643f7f6b6ee5f573526554ad273
nick-wick <65551186+nick-wick@users.noreply.github.com>
2021-04-02 11:44:46 -0400

    Added brushes

---
+++ WickBrush.js
@@ -0,0 +1,404 @@
+function defaultBrush(b) {
+    let left, right, top, bottom;
+    let r0 = b.pPressure * b.pSize;
+    let r1 = b.pressure * b.size;
+    let ctx = b.canvas.getContext('2d');
+    let pts = b.smoothNodes;
+    ctx.fillColor = b.color;
+    for (let pt of pts) {
+        let r = pt.t * r1 + (1 - pt.t) * r0;
+        ctx.beginPath();
+        ctx.moveTo(pt.x, pt.y);
+        ctx.arc(pt.x, pt.y, r, 0, 2 * Math.PI);
+        ctx.fill();
+        left = left === undefined ? pt.x - r : Math.min(left, pt.x - r);
+        right = right === undefined ? pt.x + r : Math.max(right, pt.x + r);
+        top = top === undefined ? pt.y - r : Math.min(top, pt.y - r);
+        bottom = bottom === undefined ? pt.y + r : Math.max(bottom, pt.y + r);
+    }
+    return {left: left, right: right, top: top, bottom: bottom};
+}
+
+class WickBrush {
+    constructor(args) {
+        if (!args) args = {};
+
+        this.bounds = {left: null, right: null, top: null, bottom: null};
+
+        this.changeNames = ['node', 'nodes', 'smoothNodes', 'size', 'pressure', 'color'];
+        this.handlers = {};
+        
+        this.smoothing = args.smoothing || 50; //TODO getter/setter
+        this.numNodes = args.numNodes || 10; //TODO getter/setter
+        this.tension = args.tension || 10;
+        this.catchUp = args.catchUp === undefined ? 
+            true : 
+            args.catchUp;
+        this.includeSmoothNodes = args.includeSmoothNodes === undefined ? 
+            true : 
+            args.includeSmoothNodes; //TODO getter/setter
+        this.interval = args.interval || 10; //TODO getter/setter
+        
+        this.size = args.size || 40;
+        this.pressure = 1;
+        this.color = args.fillColor || '0x000000';
+
+        this.onDown = args.onDown;
+        this.onDraw = args.onDraw;
+        this.onMove = args.onMove;
+        this.onUp = args.onUp;
+        this.onStrokeFinished = args.onStrokeFinished;
+
+        this.drawing = false;
+
+        this.brushTip = args.brushTip || defaultBrush;
+        
+        let self = this;
+        this.handlers.canvas = function(e) {
+            self.down(e);
+        };
+        this.canvas = args.canvas;
+
+        this.debug = args.debug;
+        // TODO: getters and setters for debugCanvas
+        this.debugCanvas = args.debugCanvas;
+        if (this.debugCanvas) {
+            this.debugCanvas.style.pointerEvents = 'none';
+        }
+    }
+
+    set debug(b) {
+        if (b) {
+            this._debug = true;
+        } else {
+            if (this.debugCanvas) {
+                let ctx = this.debugCanvas.getContext('2d');
+                ctx.clearRect(0,0,this.debugCanvas.width, this.debugCanvas.height);
+            }
+            this._debug = false;
+        }
+    }
+
+    get debug() {
+        return this._debug;
+    }
+
+    set canvas(canvas) {
+        this._canvas && this._canvas.removeEventListener('onpointerdown', this.handlers.canvas, false);
+        this._canvas = canvas;
+        this._canvas.addEventListener(
+            'pointerdown',
+            this.handlers.canvas,
+            false);
+    }
+
+    get canvas() {
+        return this._canvas;
+    }
+
+    updateBounds(rect) {
+        let l, r, t, b;
+        if (!rect || 
+            isNaN(rect.left) ||
+            isNaN(rect.right) ||
+            isNaN(rect.top) ||
+            isNaN(rect.bottom)) {
+            l = this.node.x;
+            r = this.node.x;
+            t = this.node.y;
+            b = this.node.y; 
+        }
+        else {
+            l = rect.left;
+            r = rect.right;
+            t = rect.top;
+            b = rect.bottom;
+        }
+
+        if (this.bounds.left !== null) {
+            this.bounds.left = Math.min(this.bounds.left, l);
+            this.bounds.right = Math.max(this.bounds.right, r);
+            this.bounds.top = Math.min(this.bounds.top, t);
+            this.bounds.bottom = Math.max(this.bounds.bottom, b);
+        }
+        else {
+            this.bounds.left = l;
+            this.bounds.right = r;
+            this.bounds.top = t;
+            this.bounds.bottom = b;
+        }
+    }
+
+    change(properties) {
+        for (const property in properties) {
+            let name = property;
+            if (this.changeNames.indexOf(name) === -1) this.changeNames.push(name);
+            //TODO: add protected values that can't be changed?
+            //would prolly just be node nodes smoothNodes
+            this[name] = properties[property];
+        }
+    }
+
+    getArgs(argNames) {
+        if (argNames) {
+            let args = {};
+            for (let i = 0; i < argNames.length; i++) {
+                let name = argNames[i];
+                args[name] = this[name];
+            }
+            return args;
+        }
+        else {
+            let args = {canvas: this.canvas};
+            for (let i = 0; i < this.changeNames.length; i++) {
+                let name = this.changeNames[i];
+                let pName = 'p' + name[0].toUpperCase() + name.substring(1);
+                args[name] = this[name];
+                args[pName] = this[pName];
+            }
+            return args;
+        }
+    }
+
+    updatePrevious() {
+        for (let n = 0; n < this.changeNames.length; n++) {
+            let name = this.changeNames[n];
+            let pName = 'p' + name[0].toUpperCase() + name.substring(1);   
+            this[pName] = this[name];
+        }
+    }
+
+    calculateSmoothNodes() {
+        this.smoothNodes = [];
+        let l = this.allNodes.length;
+        if (l < 4) return;
+        let p0 = this.allNodes[l - 4];
+        let p1 = this.allNodes[l - 3];
+        let p2 = this.allNodes[l - 2];
+        let p3 = this.allNodes[l - 1];
+        let t = 0;
+        while (t < 1) {        
+            let p = {
+                x: 0.5 * (
+                    2 * p1.x + 
+                    (-p0.x + p2.x) * t +
+                    (2*p0.x - 5*p1.x + 4*p2.x - p3.x) * t*t +
+                    (-p0.x + 3*p1.x - 3*p2.x + p3.x) * t*t*t
+                    ),
+                y: 0.5 * (
+                    2 * p1.y + 
+                    (-p0.y + p2.y) * t +
+                    (2*p0.y - 5*p1.y + 4*p2.y - p3.y) * t*t +
+                    (-p0.y + 3*p1.y - 3*p2.y + p3.y) * t*t*t
+                    ),
+                t: t    
+            };
+            this.smoothNodes.push(p);
+
+            let dpdt =  {
+                x: 0.5 * (
+                    (-p0.x + p2.x) +
+                    (4*p0.x - 10*p1.x + 8*p2.x - 2*p3.x) * t +
+                    (-3*p0.x + 9*p1.x - 9*p2.x + 3*p3.x) * t*t
+                    ),
+                y: 0.5 * (
+                    (-p0.y + p2.y) +
+                    (4*p0.y - 10*p1.y + 8*p2.y - 2*p3.y) * t +
+                    (-3*p0.y + 9*p1.y - 9*p2.y + 3*p3.y) * t*t
+                    ),
+            }
+            let dpdtMag = Math.sqrt(dpdt.x * dpdt.x + dpdt.y * dpdt.y);
+            //TODO: select step size adaptively
+            // based on radius?
+            // this.getSmoothNodesStepSize(t)?
+            let dpMag = 2;
+            let dt = dpMag / dpdtMag;
+            t += dt;
+        }
+    }
+
+    down(e) {
+        //initialize bounds
+        this.bounds = {left: null, right: null, top: null, bottom: null};
+        this.drawing = true;
+
+        //initialize nodes
+        this.nodes = [];
+        let rect = this.canvas.getBoundingClientRect();
+        let x = e.clientX - rect.left;
+        let y = e.clientY - rect.top;
+        for (let n = 0; n < this.numNodes; n++) {
+            this.nodes.push({
+                x: x,
+                y: y
+            });
+        }
+        this.node = this.nodes[this.nodes.length - 1];
+        this.allNodes = [this.node];
+        this.calculateSmoothNodes();
+
+        //initialize previous values to null
+        for (let i = 0; i < this.changeNames.length; i++) {
+            let name = this.changeNames[i];
+            let pName = 'p' + name[0].toUpperCase() + name.substring(1);   
+            this[pName] = null;
+        }
+
+        this.onDown && this.onDown(this, e);
+        let bound = this.brushTip(this);
+        this.updateBounds(bound);
+        this.debug && this.drawDebug(bound);
+        
+        // Setup events to move brush, draw brush, and lift brush
+        let self = this;
+        let moveHandler = function(e) {self.move(e)};
+        let upHandler = function(e) {self.up(e)};
+        this.handlers.pointermove = moveHandler;
+        this.handlers.pointerup = upHandler;
+        window.addEventListener('pointermove', moveHandler);
+        window.addEventListener('pointerup', upHandler);
+        let drawHandler = function() {self.draw()};
+        this.handlers.interval = setInterval(drawHandler, this.interval);
+    }
+
+    draw() {
+        if (!this.drawing) return;
+        
+        this.updatePrevious();
+
+        //update nodes
+        let newNodes = []; // needs to be a new list so pNodes is not just a shallow copy
+        let lastNode = null;
+        for (let node of this.nodes) {
+            if (lastNode) {
+                //lerp
+                let t = this.tension / 100;
+                //again, needs to be new object so it's not just a shallow copy
+                newNodes.push({
+                    x: (1 - t) * lastNode.x + t * node.x, 
+                    y: (1 - t) * lastNode.y + t * node.y});
+            }
+            else {
+                newNodes.push({
+                    x: node.x,
+                    y: node.y
+                });
+            }
+            lastNode = node;
+        }
+        this.nodes = newNodes;
+        this.node = this.nodes[this.nodes.length - 1];
+        this.allNodes.push(this.node);
+
+        if (this.includeSmoothNodes) this.calculateSmoothNodes();
+        
+        this.onDraw && this.onDraw(this);
+        let bound = this.brushTip(this);
+        this.updateBounds(bound);
+        this.debug && this.drawDebug(bound);
+    }
+
+    move(e) {
+        let r = false;
+        if (this.onMove) {
+            r = this.onMove(e);
+        }
+
+        if (!r) {
+            //update root node
+            let rect = this.canvas.getBoundingClientRect();
+            this.nodes[0].x = e.clientX - rect.left;
+            this.nodes[0].y = e.clientY - rect.top;
+        }
+    }
+
+    up(e) {
+        this.onUp && this.onUp(e, this);
+        
+        //should catch up go before onUp?
+        if (this.catchUp) {
+            while(this.nodes.length >= 2) {
+                this.updatePrevious();
+
+                this.nodes.pop();
+                this.node = this.nodes[this.nodes.length - 1]; //TODO: needs to be deep copy
+                this.allNodes.push(this.node);
+
+                this.calculateSmoothNodes();
+
+                let bound = this.brushTip(this);
+                this.updateBounds(bound);
+                this.debug && this.drawDebug(bound);
+            }
+        }
+
+        this.drawing = false;
+
+        window.removeEventListener('pointermove', this.handlers.pointermove);
+        window.removeEventListener('pointerup', this.handlers.pointerup);
+        clearInterval(this.handlers.interval);
+
+        this.onStrokeFinished && this.onStrokeFinished(this);
+    }
+
+    cancel() {
+        this.drawing = false;
+
+        window.removeEventListener('pointermove', this.move);
+        window.removeEventListener('pointerup', this.up);
+        clearInterval(this.updateId);
+
+        //this.bounds = {x: undefined, y: undefined, width: 0, height: 0};
+    }
+
+    drawDebug(bound) {
+        if (!this.debugCanvas) return;
+        let ctx = this.debugCanvas.getContext('2d');
+        ctx.clearRect(0, 0, this.debugCanvas.width, this.debugCanvas.height);
+        
+        //bound
+        ctx.strokeStyle = '#FF0000';
+        if (bound) {
+            
+            ctx.beginPath();
+            ctx.rect(bound.left, 
+                bound.top, 
+                bound.right - bound.left, 
+                bound.bottom - bound.top);
+            ctx.stroke();
+        }
+        //bounds
+        ctx.beginPath();
+        ctx.rect(this.bounds.left,
+            this.bounds.top,
+            this.bounds.right - this.bounds.left,
+            this.bounds.bottom - this.bounds.top);
+        ctx.stroke();
+
+        //nodes
+        ctx.strokeStyle = '#00FF00';
+        for (let n = 0; n < this.nodes.length; n++) {
+            let node = this.nodes[n];
+            
+            ctx.beginPath();
+            ctx.arc(node.x, node.y,5,0,2*Math.PI);
+            ctx.stroke();
+        }
+
+        //smoothNodes
+        ctx.strokeStyle = '#00FFFF';
+        for (let n = 0; n < this.smoothNodes.length; n++) {
+            let node = this.smoothNodes[n];
+
+            ctx.beginPath();
+            ctx.arc(node.x, node.y,10,0,2*Math.PI);
+            ctx.stroke();
+        }
+    }
+
+    cleanup() {
+        if (this.drawing) this.cancel();
+
+        this.canvas.removeEventListener('onpointerdown', this.down, false);
+    }
+}
---
+++ brushes.js
@@ -0,0 +1,134 @@
+//a*x1 + b*x2
+function add(x1, x2) {
+    return {x: x1.x + x2.x, y: x1.y + x2.y};
+}
+
+function mag(x) {
+    return Math.sqrt(x.x*x.x + x.y*x.y);
+}
+
+function scale(a, x) {
+    return {x: a*x.x, y: a*x.y};
+}
+
+function rot(x) {
+    return {x: -x.y, y: x.x};
+}
+
+let tireLength = 20;
+let tireWidth = 10;
+function tire(b) {
+    if (!b.tireTime) b.tireTime = 0;
+
+    //let left, right, top, bottom;
+
+    let r = b.size;
+    let ctx = b.canvas.getContext('2d');
+    ctx.fillStyle = b.color;
+    if (b.pSmoothNodes) {
+        let l = b.pSmoothNodes.length;
+        l >= 1 && b.smoothNodes.unshift(b.pSmoothNodes[l - 1]);
+        l >= 2 && b.smoothNodes.unshift(b.pSmoothNodes[l - 2]);
+        l >= 3 && b.smoothNodes.unshift(b.pSmoothNodes[l - 3]);
+    }
+    let pts = b.smoothNodes;
+
+    for (let p = 3; p < pts.length; p++) {
+        
+        let p0 = pts[p-3];
+        let p1 = pts[p-2];
+        let p2 = pts[p-1]
+        let p3 = pts[p];
+
+        let p12 = add(p2, scale(-1, p1));
+        let dTireTime = mag(p12);
+        let t1 = b.tireTime;
+        let t2 = b.tireTime + dTireTime;
+        b.tireTime = t2;
+
+        let dp1 = add(p2, scale(-1, p0));
+        let dp1Mag = mag(dp1);
+        let dp2 = add(p3, scale(-1, p1));
+        let dp2Mag = mag(dp2);
+
+        let tp1 = rot(scale(tireWidth/2/dp1Mag, dp1));
+        let tp2 = rot(scale(tireWidth/2/dp2Mag, dp2));
+
+        let C0 = add(p1, scale(-1, tp1));
+        let C1 = add(p1, tp1);
+        let C2 = add(p2, scale(-1, tp2));
+        let C3 = add(p2, tp2);
+
+        let t = t1;
+        let i = Math.floor(t / tireLength);
+        while (t < t2) {
+            let nextT = Math.min(t2, (i + 1) * tireLength);
+
+            let T1 = (t - t1) / (t2 - t1);
+            let T2 = (nextT - t1) / (t2 - t1);
+            
+            let c0 = add(scale(1 - T1, C0), scale(T1, C2));
+            let c1 = add(scale(1 - T1, C1), scale(T1, C3));
+            let c2 = add(scale(1 - T2, C0), scale(T2, C2));
+            let c3 = add(scale(1 - T2, C1), scale(T2, C3));
+            let c001 = add(scale(2/3, c0), scale(1/3, c1)), 
+            c011 = add(scale(1/3, c0), scale(2/3, c1)), 
+            c223 = add(scale(2/3, c2), scale(1/3, c3)), 
+            c233 = add(scale(1/3, c2), scale(2/3, c3));
+
+            if (i % 2 == 0) {
+                //middle tread
+                
+                ctx.beginPath();
+                ctx.moveTo(c001.x, c001.y);
+                ctx.lineTo(c011.x, c011.y);
+                ctx.lineTo(c233.x, c233.y);
+                ctx.lineTo(c223.x, c223.y);
+                ctx.closePath();
+                ctx.fill();
+            }
+            else {
+                //outer treads
+                ctx.beginPath();
+                ctx.moveTo(c001.x, c001.y);
+                ctx.lineTo(c223.x, c223.y);
+                ctx.lineTo(c2.x, c2.y);
+                ctx.lineTo(c0.x, c0.y);
+                ctx.closePath();
+                ctx.fill();
+
+                ctx.beginPath();
+                ctx.moveTo(c011.x, c011.y);
+                ctx.lineTo(c233.x, c233.y);
+                ctx.lineTo(c3.x, c3.y);
+                ctx.lineTo(c1.x, c1.y);
+                ctx.closePath();
+                ctx.fill();
+            }
+
+            t = nextT;
+            i = Math.floor(t / tireLength);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+function stampTip(b) {
+    let ctx = b.canvas.getContext('2d');
+    let s = b.stampCanvas;
+    
+    for (let i = 0; i < b.smoothNodes.length; i++) {
+        let node = b.smoothNodes[i];
+        ctx.drawImage(s, 0, 0, s.width, s.height, node.x - b.size/2, node.y - b.size/2, b.size, b.size);
+    }
+    
+}
