0c30475b88876db4aa6a1101f606925aa95c2101
nick-wick <65551186+nick-wick@users.noreply.github.com>
2021-04-26 09:36:01 -0400

    Updating examples page

--- docs/script.js
+++ docs/script.js
@@ -1,15 +1,15 @@
-let embed = document.getElementById("embed");
-let select = document.getElementById("select");
-console.log(select);
+// let embed = document.getElementById("embed");
+// let select = document.getElementById("select");
+// console.log(select);
 
-embed.width = window.innerWidth;
-embed.height = window.innerHeight;
+// embed.width = window.innerWidth;
+// embed.height = window.innerHeight;
 
-let srcs = ["smoothBrush/index.html", "stampBrush/index.html"];
-let i = 0;
-select.addEventListener("click", function(){
-    console.log('click');
-    i++;
-    i = i % srcs.length;
-    embed.setAttribute("src", srcs[i]);
-})
+// let srcs = ["smoothBrush/index.html", "stampBrush/index.html"];
+// let i = 0;
+// select.addEventListener("click", function(){
+//     console.log('click');
+//     i++;
+//     i = i % srcs.length;
+//     embed.setAttribute("src", srcs[i]);
+// })
