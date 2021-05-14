eeb50f89445df544fdbe76f3f3344907135162e7
nick-wick <65551186+nick-wick@users.noreply.github.com>
2021-05-14 10:48:43 -0400

    Add stamp brush

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,25 @@
 let clear = document.getElementById("clear");
 let pressure = document.getElementById("pressure");
-const toolNames = ["clear", "pressure"];
+let stampPressure = document.getElementById("stampPressure");
+let color1 = document.getElementById("color1");
+const toolNames = ["clear", "pressure", "stampPressure", "color1"];
+
+function showElements(elts) {
+    for (let toolName of toolNames) {
+        if (elts[toolName]) {
+            document.getElementById(toolName + "Tool").style.display = "block";
+        }
+        else {
+            document.getElementById(toolName + "Tool").style.display = "none";
+        }
+    }
+    if (elts.stampCanvas) {
+        stampCanvas.style.display = "block";
+    }
+    else {
+        stampCanvas.style.display = "none";
+    }
+}
 
 let brushCanvas2 = document.getElementById("brushCanvas2");
 let brushCanvas1 = document.getElementById("brushCanvas1");
@@ -13,12 +32,44 @@ function resizeCanvases() {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
     }
+    stampCanvas.width = 200;
+    stampCanvas.height = 200;
 }
 
 resizeCanvases();
 window.onresize = resizeCanvases;
+showElements({clear: true, pressure: true, color1: true});
 
-debugCanvas.style.display = "none";
+let brush = new WickBrush({
+    canvas: brushCanvas1,
+    debugCanvas: debugCanvas,
+});
+let brush2 = new WickBrush({
+    canvas: stampCanvas,
+})
+
+let smoothBrushButton = document.getElementById("smoothBrush");
+let tireBrushButton = document.getElementById("tireBrush");
+let stampBrushButton = document.getElementById("stampBrush");
+
+smoothBrushButton.onclick = function() {
+    brush.canvas = brushCanvas1;
+    brush.brushTip = defaultBrush;
+    showElements({clear: true, pressure: true, color1: true})
+}
+
+tireBrushButton.onclick = function() {
+    brush.canvas = brushCanvas1;
+    brush.brushTip = tire;
+    showElements({clear: true, color1: true});
+}
+
+stampBrushButton.onclick = function() {
+    brush.canvas = brushCanvas1;
+    brush.brushTip = stampTip;
+    brush.stampCanvas = stampCanvas;
+    showElements({clear: true, pressure: true, stampPressure: true, color1: true, stampCanvas: true})
+}
 
 clear.onclick = function() {
     for (let canvas of canvases) {
@@ -27,33 +78,51 @@ clear.onclick = function() {
     }
 }
 
-let smoothBrushButton = document.getElementById("smoothBrush");
-let tireBrushButton = document.getElementById("tireBrush");
-
-let brush = new WickBrush({
-    canvas: brushCanvas1,
-    debugCanvas: debugCanvas,
+let force = 1;
+Pressure.set(brushCanvas1, {
+    change: function(f, event){
+        force = f;
+    }
 });
-
-smoothBrushButton.onclick = function() {
-    brush.canvas = brushCanvas1;
-    brush.brushTip = defaultBrush;
-    showElements({clear: true, pressure: true, fillColor: true,})
-}
-
-tireBrushButton.onclick = function() {
-    brush.canvas = brushCanvas1;
-    brush.brushTip = tire;
-    showElements({clear: true, fillColor: true})
-}
-
-function showElements(elts) {
-    for (let toolName of toolNames) {
-        if (elts[toolName]) {
-            document.getElementById(toolName + "Tool").style.display = "block";
+pressure.onchange = function(e) {
+    if (e.target.checked) {
+        brush.onDown = function() {
+            brush.change({pressure: 0});
         }
-        else {
-            document.getElementById(toolName + "Tool").style.display = "none";
+        brush.onDraw = function() {
+            brush.change({pressure: Math.max(force, .03)});
         }
     }
+    else {
+        brush.onDown = null;
+        brush.onDraw = null;
+        brush.pressure = 1;
+    }
+}
+
+let stampForce = 1;
+Pressure.set(stampCanvas, {
+    change: function(f, event){
+        stampForce = f;
+    }
+});
+stampPressure.onchange = function(e) {
+    if (e.target.checked) {
+        brush2.onDown = function() {
+            brush2.change({pressure: 0});
+        }
+        brush2.onDraw = function() {
+            brush2.change({pressure: Math.max(stampForce, .03)});
+        }
+    }
+    else {
+        brush2.onDown = null;
+        brush2.onDraw = null;
+        brush2.pressure = 1;
+    }
+}
+
+color1.onchange = function(e) {
+    brush.fillStyle = e.target.value;
+    brush2.fillStyle = e.target.value;
 }
